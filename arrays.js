3bed58a22dce538404785084acb5bf61f0d90b30
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-22 21:38:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray(arr, elem){
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, elem){
-  return arr.unshift(elem)
+  arr.unshift(elem)
+  return arr
 }
 
 function addElementToEndOfArray(arr, elem){
